c9c0caa20f1accbbae67d181116ce365ad2f1f1f
paschalugwu <ugwupaschal@gmail.com>
2023-08-15 16:37:19 -0700

    Write a function that checks for lowercase character.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,5 +4,6 @@
 void _putchar(char c);  /* Prototype for _putchar */
 void print_alphabet(void);  /* Prototype for print_alphabet */
 void print_alphabet_x10(void);  /* Prototype for print_alphabet_x10 */
+int _islower(int c);    /* Prototype for _islower */
 
 #endif /* MAIN_H */
