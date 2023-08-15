3d43720cdeb6b838d855c0eaa3c61793d89d32e4
paschalugwu <ugwupaschal@gmail.com>
2023-08-15 16:48:07 -0700

    Write a function that checks for alphabetic character.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,6 @@ void _putchar(char c);  /* Prototype for _putchar */
 void print_alphabet(void);  /* Prototype for print_alphabet */
 void print_alphabet_x10(void);  /* Prototype for print_alphabet_x10 */
 int _islower(int c);    /* Prototype for _islower */
+int _isalpha(int c);    /* Prototype for _isalpha */
 
 #endif /* MAIN_H */
