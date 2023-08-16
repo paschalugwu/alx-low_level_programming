6455e29196db08e9c9fb408f3810bd5044644fb7
paschalugwu <ugwupaschal@gmail.com>
2023-08-15 17:38:29 -0700

    Write a function that computes the absolute value of an integer.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,5 +7,6 @@ void print_alphabet_x10(void);  /* Prototype for print_alphabet_x10 */
 int _islower(int c);    /* Prototype for _islower */
 int _isalpha(int c);    /* Prototype for _isalpha */
 int print_sign(int n);  /* Prototype for print_sign */
+int _abs(int n);        /* Prototype for _abs */
 
 #endif /* MAIN_H */
