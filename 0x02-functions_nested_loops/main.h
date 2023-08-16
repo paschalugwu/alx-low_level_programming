7e83a9a9c373b340e43325dd9fb48e565cf40678
paschalugwu <ugwupaschal@gmail.com>
2023-08-15 17:29:02 -0700

    Write a function that prints the sign of a number.

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,5 +6,6 @@ void print_alphabet(void);  /* Prototype for print_alphabet */
 void print_alphabet_x10(void);  /* Prototype for print_alphabet_x10 */
 int _islower(int c);    /* Prototype for _islower */
 int _isalpha(int c);    /* Prototype for _isalpha */
+int print_sign(int n);  /* Prototype for print_sign */
 
 #endif /* MAIN_H */
