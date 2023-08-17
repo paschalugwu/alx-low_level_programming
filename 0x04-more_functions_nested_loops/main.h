1dc9d9c135880193d0ed55311a74a66a39cfc572
paschalugwu <ugwupaschal@gmail.com>
2023-08-17 09:15:43 -0700

    Write a function that prints a triangle, followed by a new line.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -12,5 +14,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void fizz_buzz(void);
+void print_triangle(int size);
 
 #endif /* MAIN_H */
