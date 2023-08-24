bce40f90f69fa9bf8a6b3835b7bffbaa6ad50d64
paschalugwu <ugwupaschal@gmail.com>
2023-08-24 05:51:03 -0700

    Write a function that prints an integer.

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,28 +6,23 @@
  */
 void print_digit(int n)
 {
-	if (n / 10 != 0)
-		print_digit(n / 10);
-	_putchar(n % 10 + '0');
+	_putchar(n + '0');
 }
 
 /**
  * print_number - Prints an integer
  * @n: The number to print
  */
-void print_number(int n)
+void print_number(int n) /* Recursive function to print a number */
 {
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
-	if (n == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		print_digit(n);
-	}
+
+	if (n / 10 != 0)
+		print_number(n / 10);
+
+	print_digit(n % 10);
 }
