381c98b6026fbdd097bc38381693472c43f791d4
root <root@DESKTOP-CDE41BB.localdomain>
2023-08-28 14:22:55 +0100

    Write a function that locates a character in a string.

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to search.
+ * @c: The character to locate.
+ *
+ * Return: If c is found - a pointer to first occurence.
+ * If c is not found - NULL
+*/
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+
+	if (*s == c)
+		return (s);
+
+	return (NULL);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,7 @@
 char *_memset(char *s, char b, unsigned int n);
 /* Function prototype for _memcpy */
 char *_memcpy(char *dest, char *scr, unsigned int n);
+/* Function prototype for strchr */
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
