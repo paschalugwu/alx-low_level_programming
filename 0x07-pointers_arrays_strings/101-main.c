8ba03a9a0233eff95e8a11b79a288466815317f3
root <root@DESKTOP-CDE41BB.localdomain>
2023-08-28 21:34:43 +0100

    Create a file that contains the password for the crackme2 executable.

---
+++ 0x07-pointers_arrays_strings/101-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char *str;
+
+	str = "Hello, world!";
+	_puts(str);
+
+	return (0);
+}
