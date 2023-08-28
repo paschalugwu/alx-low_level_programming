792a53158b1fa061e8aadc237998c2158046b1ed
root <root@DESKTOP-CDE41BB.localdomain>
2023-08-28 13:21:54 +0100

    Write a function that copies memory area.

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,10 +1,10 @@
 /**
  * _memcpy - Copies memory are
  * @dest: Pointer to the destinatio memory area
- * @src: Pointer to the source memory are
+ * @src: Pointer to the source memory area
  * @n: Number of bytes to be copied
  *
- * Reurn: Pointer to the destination memory area (dest)
+ * Return: Pointer to the destination memory area (dest)
  * or NULL if an error occurs
 */
 char *_memcpy(char *dest, char *src, unsigned int n)
