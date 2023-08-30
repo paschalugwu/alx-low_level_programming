8f8380cb6cca20bc389116eca7cd90879132b65a
root <root@DESKTOP-CDE41BB.localdomain>
2023-08-30 17:18:12 +0100

    Write a function that prints a string in reverse.

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,7 @@
 /* Prototype to print a string, followed by a new line. */
 void _puts_recursion(char *s);
 int _putchar(char c);
-
+/* Prototype to prints a string in reverse. */
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
