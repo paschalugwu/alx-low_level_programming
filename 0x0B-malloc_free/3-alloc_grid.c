c50a7e3cb5e523ddf23c10ff8ce5c954209cac77
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-04 20:29:41 +0100

    feat(alloc_grid): Implement function to allocate a 2D array
    This commit adds the implementation of the alloc_grid function in alloc_grid.c. The alloc_grid function takes two parameters: height and width, and dynamically allocates memory for a 2D array with the specified dimensions.
    
    The function follows the requirements specified in the task and returns a pointer to the newly allocated 2D array. If there is insufficient memory or if the dimensions are invalid, the function returns NULL.
    
    This commit also includes a corresponding update to the main.h header file to include the prototype of the alloc_grid function.
    
    Closes #3. If you even dream of beating me you'd better wake up and apologize

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Allocates a 2D array of integers
+ * @width: The width of the grid
+ * @height: The height of the grid
+ *
+ * Return: Pointer to the allocated 2D array, or NULL on failure
+*/
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grid = malloc(sizeof(int *) * height);
+
+	if (grid == (NULL))
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(grid[j]);
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			grid[i][j] = 0;
+	}
+
+	return (grid);
+}
