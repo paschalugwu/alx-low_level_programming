01d197d06e71d4bccb105d379d4cb563e19c350a
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-07 15:19:17 +0100

    feat(2-calloc): Implement _calloc function
    Implemented the _calloc function in the 2-calloc.c file to allocate memory for an array using malloc. The function follows the requirements specified in the task, including handling cases where nmemb or size is 0 and setting the allocated memory to zero. Also included a simple_print_buffer function in the 2-main.c file to test the functionality of _calloc. All tests passed successfully.
      Added documentation and comments to improve code readability and maintainability.

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocates memory for an array using malloc
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element in bytes
+ *
+ * Return: Pointer to the allocated memory, or NULL if allocation fails
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		ptr[i] = 0;
+
+	return (ptr);
+}
