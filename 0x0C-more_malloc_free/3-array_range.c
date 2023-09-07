db2b02b7b6858e548f416785f8ae30d69acd5c38
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-07 15:54:39 +0100

    feat(array_range): Implement efficient array creation function
    Implemented the  array_range  function in  3-array_range.c  to efficiently create an array of integers. The function takes in a minimum value and a maximum value and creates an array containing all the values from the minimum to the maximum, inclusive. This implementation strictly follows the given requirements, including the use of  malloc  for memory allocation and proper error handling. The function has been tested successfully with the provided  main.c  file.

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of integers
+ * @min: the minimum value
+ * @max: the maximum value
+ *
+ * Return: pointer to the newly created array
+*/
+int *array_range(int min, int max)
+{
+	int *arr, i, size;
+
+	if (min > max)
+		return (NULL);
+
+	size = max - min + 1;
+	arr = malloc(sizeof(int) * size);
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		arr[i] = min++;
+
+	return (arr);
+}
