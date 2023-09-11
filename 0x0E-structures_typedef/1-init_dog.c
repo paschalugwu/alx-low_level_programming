54ab5e0050cb8a0d4c15f6b6ef09019075312967
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-11 18:28:05 +0100

    feat: Add initialization of dog structure
    Implement the initialization of the dog structure in the files 1-init_dog.c and 1-main.c. This allows for proper initialization of the dog's name, age, and owner.

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,22 +2,18 @@
 #include <stdlib.h>
 
 /**
- * init_dog-Initializes a struct dog
- * @d: Pointer to the struct dog to initialize
+ * init_dog - Initializes a variable of type struct dog
+ * @d: Pointer to the struct dog variable to initialize
  * @name: Name of the dog
  * @age: Age of the dog
- * @owner: Owner of the dog
- *
- * Description: This function initializes a struct dog with the provided
- * name, age, and age.
- * Return: None
-*/
+ * @owner: Name of the owner of the dog
+ */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
+		(*d).name = name;
+		(*d).age = age;
+		(*d).owner = owner;
 	}
 }
