cd8df0d00ab231b5a6f927e10f5c002549b89a7e
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-11 15:17:15 +0100

    feat(3-function_like_macro.h): Add ABS function-like macro for computing absolute value
    Implement the ABS function-like macro in the 3-function_like_macro.h header file. This macro calculates the absolute value of a number using a ternary operator. It checks if the number is less than 0 and returns the negative value if true, otherwise, it returns the number itself. The ABS macro provides a convenient way to compute absolute values in the codebase.

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -3,12 +3,12 @@
 
 /**
  * main - Entry point of the program
- * 
+ *
  * This program demonstrates the usage of the ABS macro defined in the
  * "3-function_like_macro.h" header file. It calculates the absolute
  * values of two numbers, -98 and 98, multiplied by 10 using the ABS macro.
  * The results are printed using printf.
- * 
+ *
  * Return: Always 0 (success)
 */
 int main(void)
