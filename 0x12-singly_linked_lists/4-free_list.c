459f454705171b7f4dde5f9e6d499398d127299c
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-21 16:33:32 +0100

    fix(free_list): Resolve implicit declaration error for free function
    This commit fixes the implicit declaration error for the  free  function in the 4-free_list.c file. The error occurred because the necessary  stdlib.h  header file was missing, resulting in an implicit declaration of the  free  function. The fix involves including the  stdlib.h  header file at the beginning of the file to provide the proper declaration for the  free  function. This resolves the error and ensures that the code can be compiled successfully. The code has been reviewed and tested to ensure its correctness and adherence to the specified guidelines.

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - Frees a list_t list
+ * @head: Pointer to the head of the list
+ *
+ * Return: void
+*/
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
