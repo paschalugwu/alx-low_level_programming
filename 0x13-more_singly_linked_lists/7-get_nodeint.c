7bdc1c35a6f1f3b2c3a7d254b40c475c80c14020
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-25 21:46:02 +0100

    feat(get_nodeint_at_index): Implement function to retrieve node at given index in a linked list
    This commit includes the implementation of the  get_nodeint_at_index  function in the  7-get_nodeint.c  file. The function takes a pointer to the head of a linked list of type  listint_t  and an index as parameters. It retrieves the node at the given index in the list and returns a pointer to that node.
    
    The  main  function in the  7-main.c  file is also added to test the functionality of the  get_nodeint_at_index  function. It creates a linked list, adds nodes to it, and then retrieves the node at index 5. The value of the retrieved node is printed, along with the elements of the linked list before and after the retrieval.
    
    Both files adhere to the given requirements and guidelines for this task, including the use of block comments, adherence to the Betty style, and the inclusion of the necessary header file  lists.h  that contains the definition of the  listint_t  structure and the function prototypes.

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Returns the nth of a listint_t linked list.
+ * @head: Pointer to the head of the list.
+ * @index: Index of the node, starting at 0.
+ *
+ * Return: The nth node of the list, or NULL if it does not exist.
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *current = head;
+
+	while (current != NULL)
+	{
+		if (count == index)
+			return (current);
+		count++;
+		current = current->next;
+	}
+	return (NULL);
+}
