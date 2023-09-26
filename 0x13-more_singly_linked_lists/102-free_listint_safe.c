e655e222b5745ca6bef5375ba590e32c62d8d862
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-26 10:08:45 +0100

    feat(free_listint_safe): Implement free_listint_safe function
    This commit adds the implementation of the free_listint_safe function. The function frees a listint_t linked list, handling lists with loops efficiently. It goes through the list only once and sets the head to NULL. The function returns the size of the list that was freed.
    
    The code follows the given requirements, including the use of block comments, adherence to the Betty style, and limited use of C standard library functions. It has been tested and verified to work correctly.

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -2,33 +2,32 @@
 #include <stdlib.h>
 
 /**
- * free_listint_safe - Frees a listint_t list
- * @h: Pointer to the head of the list
+ * free_listint_safe - Frees a listint_t list safely
+ * @h: A pointer to the head of the list
  *
  * Return: The size of the list that was freed
  */
 size_t free_listint_safe(listint_t **h)
 {
-	listint_t *current, *temp;
-	size_t count = 0;
+	listint_t *current = *h;
+	listint_t *temp;
+	size_t size = 0;
 
-	if (h == NULL || *h == NULL)
-		return (0);
-
-	current = *h;
-	while (current != NULL)
+	while (current)
 	{
-		count++;
-		if (current->next >= current)
+		temp = current->next;
+		free(current);
+		size++;
+
+		/* Detect a loop */
+		if (temp >= current)
 		{
 			*h = NULL;
-			return (count);
+			return (size);
 		}
-		temp = current;
-		current = current->next;
-		free(temp);
+		current = temp;
 	}
 
 	*h = NULL;
-	return (count);
+	return (size);
 }
