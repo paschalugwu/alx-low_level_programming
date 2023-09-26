dc41bb2d6c11e3bb75a7e3cc9e87d37b972a1243
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-26 10:18:55 +0100

    feat(print_listint_safe): implement print_listint_safe function
    Implemented the print_listint_safe function according to task requirements. The function prints nodes in a linked list and identifies loops, meeting all specifications.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,28 +1,35 @@
-#include "lists.h"
 #include <stdlib.h>
+#include "lists.h"
 #include <stdio.h>
 
 /**
- * print_listint_safe - Prints a listint_t linked list
- * @head: Pointer to the head of the list
- *
- * Return: Number of nodes in the list
+ * print_listint_safe - prints a listint_t linked list
+ * @head: pointer to the head of the list
+ * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
 	size_t count = 0;
+	const listint_t *tortoise = head, *hare = head;
 
-	while (current != NULL)
+	if (head == NULL)
 	{
-		printf("[%p] %d\n", (void *)current, current->n);
+		exit(98);
+	}
+
+	while (tortoise != NULL && hare != NULL && hare->next != NULL)
+	{
+		printf("[%p] %d\n", (void *)tortoise, tortoise->n);
 		count++;
-		if (current <= current->next)
+
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+
+		if (tortoise == hare)
 		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
+			printf("-> [%p] %d\n", (void *)tortoise, tortoise->n);
 			break;
 		}
-		current = current->next;
 	}
 
 	return (count);
