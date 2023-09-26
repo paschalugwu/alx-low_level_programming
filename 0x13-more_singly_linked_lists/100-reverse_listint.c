948eb8578f73e90631773782b598b9cc1c1b56a7
root <root@DESKTOP-CDE41BB.localdomain>
2023-09-26 06:58:37 +0100

    [100-reverse_listint.c] Completed task: Reverse list
    This commit implements the  reverse_listint  function, which reverses a given  listint_t  linked list. The function adheres to the provided requirements: it uses only one loop, declares a maximum of two variables, and does not use  malloc ,  free , or arrays.
    
    To test the functionality, a  main  function is provided in  100-main.c . It creates a sample linked list using the  add_nodeint_end  function, prints the original list using  print_listint , calls  reverse_listint  to reverse the list, and then prints the reversed list again.
    
    The code has been successfully compiled and tested using  gcc  and  valgrind , ensuring memory correctness. All heap blocks have been freed, and no memory leaks are possible.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -2,22 +2,20 @@
 
 /**
  * reverse_listint - Reverses a listint_t linked list.
- * @head: Pointer to the head of the list.
+ * @head: A pointer to the head of the list.
  *
- * Return: Pointer to the first node of the reversed list.
+ * Return: A pointer to the first node of the reversed list.
  */
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	listint_t *next = NULL;
+	listint_t *prev = NULL, *next = NULL;
 
-	while (current != NULL)
+	while (*head != NULL)
 	{
-		next = current->next;
-		current->next = prev;
-		prev = current;
-		current = next;
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
 	}
 
 	*head = prev;
