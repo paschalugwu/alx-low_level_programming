cdd7e165841a3bec300b116ef9348dd487ae8830
root <root@DESKTOP-CDE41BB.localdomain>
2023-10-02 18:59:49 +0100

    feat(test): Add 3-main.c file for testing 3-cp.c
    Add the 3-main.c file which contains a main function to test the functionality of the 3-cp.c code. The main function checks the command line arguments and calls the my_cp() function. It also provides output messages based on the success or failure of the copy operation.

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of command-line arguments
+ * @argv: Array containing the command-line arguments
+ *
+ * Return: 0 on success, other values on failure
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, rd, wr;
+	char buffer[1024];
+
+	if (argc != 3)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"),
+		exit(97);
+
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]), exit(98);
+
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+			argv[2]), exit(99);
+
+	while ((rd = read(file_from, buffer, 1024)) > 0)
+	{
+		wr = write(file_to, buffer, rd);
+		if (wr != rd || wr == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n",
+				argv[2]), exit(99);
+	}
+
+	if (rd == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n",
+			argv[1]), exit(98);
+
+	if (close(file_from) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			file_from), exit(100);
+
+	if (close(file_to) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			file_to), exit(100);
+
+	return (0);
+}
